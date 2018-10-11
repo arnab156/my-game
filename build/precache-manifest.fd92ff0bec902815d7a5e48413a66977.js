6cba9f2b17e4be30e864ee4b7f398efc0477d644
arnab majumdar <arnab156@gmail.com>
2018-10-11 18:12:53 -0500

    deployment

---
+++ build/precache-manifest.fd92ff0bec902815d7a5e48413a66977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b1d4186f152753c596f",
+    "url": "/my-game/static/js/runtime~main.6b1d4186.js"
+  },
+  {
+    "revision": "f7c9193981ccb54e1798",
+    "url": "/my-game/static/js/main.f7c91939.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/my-game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "f7c9193981ccb54e1798",
+    "url": "/my-game/static/css/main.bfa23d43.chunk.css"
+  },
+  {
+    "revision": "406e73b98ced88a1fe9610ae50040451",
+    "url": "/my-game/index.html"
+  }
+];
